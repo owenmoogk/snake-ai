be5da3955d10461f08fa706d1f3fdfc95800d585
owenmoogk <owenmoogk@gmail.com>
2021-01-07 01:35:07 -0500

    it works!!!

--- main.py
+++ main.py
@@ -26,10 +26,9 @@ font = pygame.font.SysFont("comicsans", 50)
 score = 0
 gridSize = 20
 gameSpeed = 7
-
-# sprites
-snakeSquares = []
-
+gridHeight = windowHeight / gridSize
+gridWidth = windowWidth / gridSize
+snakeSquares = [] # keeps the snake square objects
 class snake:
     def __init__(self, x, y, direction):
         self.x = x * gridSize
@@ -38,9 +37,6 @@ class snake:
     
     def drawSquare(self):
         pygame.draw.rect(screen, green, (self.x,self.y,gridSize, gridSize))
-
-    def death(self):
-        print("u died")
 
     def move(self):
         if self.direction == "left":
@@ -51,13 +47,9 @@ class snake:
             self.y -= gridSize
         elif self.direction == "down":
             self.y += gridSize
+        if self.y < 0 or self.y >= windowHeight or self.x < 0 or self.x >= windowWidth:
+            return(True)
         
-        if self.y < 0 or self.y >= windowHeight:
-            self.death()
-        elif self.x < 0 or self.x >= windowWidth:
-            self.death()
-        
-    
     def changeDirection(self, direction):
         if self.direction == "up" and direction == "down":
             return
@@ -71,56 +63,90 @@ class snake:
             self.direction = direction
 
 class food:
-    def __init__(self, x, y):
-        self.x = x * gridSize
-        self.y = y * gridSize
+    def __init__(self):
+        self.spawnFood()
     
     def drawFood(self):
         pygame.draw.rect(screen, red, (self.x,self.y, gridSize, gridSize))
+    
+    def spawnFood(self):
+        improper = True
+        while improper:
+            self.x = randint(0,gridWidth-1) * gridSize
+            self.y = randint(0,gridHeight-1) * gridSize
+            improper = False
+            for i in snakeSquares:
+                if self.x == i.x and self.y == i.y:
+                    improper = True
+                    break
 
 def showScore(score):
     score_label = font.render("Score: " + str(score),1,(255,255,255))
     screen.blit(score_label, (10, 10))
 
-snakeSquares.append(snake(4,windowHeight/2/gridSize-1,"right"))
-
 running = True
-backlog = []
 
 while running:
 
-    # running thru events
-    events = pygame.event.get()
-    for event in events:
-        # if x button pressed stop just break out of these loops
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                backlog.append("up")
-            elif event.key == pygame.K_DOWN:
-                backlog.append("down")
-            elif event.key == pygame.K_LEFT:
-                backlog.append("left")
-            elif event.key == pygame.K_RIGHT:
-                backlog.append("right")
-    
-    # if backlog has elements on it, then execute the first (every loop)
-    if len(backlog) > 0:
-        snakeSquares[0].changeDirection(backlog[0])
-        del(backlog[0])
-    print(backlog)
+    playing = True
+    backlog = []
+    apple = food()
+    apple.spawnFood()
+    snakeSquares = [] # keeps the snake square objects
+    snakeSquares.append(snake(4,round(windowHeight/gridSize/2),"right"))
+    length = 1 # keeps the length of the snake
 
-    snakeSquares[0].move()
-    
-    apple = food(10,10)
+    while playing:
+        clock.tick(gameSpeed)
+        pygame.draw.rect(screen, black, (0,0,windowWidth, windowHeight))
 
-    # drawing
-    pygame.draw.rect(screen, black, (0,0,windowWidth, windowHeight))
-    snakeSquares[0].drawSquare()
-    apple.drawFood()
+        # running thru events
+        events = pygame.event.get()
+        for event in events:
+            # if x button pressed stop just break out of these loops
+            if event.type == pygame.QUIT:
+                running = False
+                playing = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    backlog.append("up")
+                elif event.key == pygame.K_DOWN:
+                    backlog.append("down")
+                elif event.key == pygame.K_LEFT:
+                    backlog.append("left")
+                elif event.key == pygame.K_RIGHT:
+                    backlog.append("right")
+        
+        # if backlog has elements on it, then execute the first (every loop)
+        if len(backlog) > 0:
+            snakeSquares[0].changeDirection(backlog[0])
+            del(backlog[0])
+        
+        if len(snakeSquares) < length:
+            snakeSquares.append(snake(0,0,"left")) # the left doesnt matter
 
-    # update display
-    clock.tick(gameSpeed)
-    showScore(score)
-    pygame.display.update()
+        # "moving" the body
+        for i in range(len(snakeSquares)-1,0,-1):
+            snakeSquares[i].x = snakeSquares[i-1].x
+            snakeSquares[i].y = snakeSquares[i-1].y
+        
+        # moving the head
+        isDead = snakeSquares[0].move()
+        if isDead:
+            playing = False
+
+        # collision detection
+        if snakeSquares[0].x == apple.x and snakeSquares[0].y == apple.y:
+            apple.spawnFood()
+            length += 1
+        for i in range(1, len(snakeSquares)):
+            if snakeSquares[0].x == snakeSquares[i].x and snakeSquares[0].y == snakeSquares[i].y:
+                playing = False
+
+        # drawing
+        apple.drawFood()
+        for i in snakeSquares:
+            i.drawSquare()
+
+        # update display
+        pygame.display.update()
